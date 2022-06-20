88b00e6124cb7b59d98a23245341657d9a858fc6
jleecounsellor <jleecounsellor@gmail.com>
2022-06-19 18:53:10 -0700

    Completed form validation and error messaging

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
+const {
+  check,
+  validationResult
+} = require("express-validator");
+
 const mongoose = require("mongoose");
 
 
@@ -24,85 +29,121 @@ const Message = mongoose.model("Message", messageSchema);
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 //home route
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
 //standard garland route
-app.get("/standard-garland", function(req, res){
+app.get("/standard-garland", function(req, res) {
   res.render("standard-garland");
 });
 
 //luxury garland route
-app.get("/luxury-garland", function(req, res){
+app.get("/luxury-garland", function(req, res) {
   res.render("luxury-garland");
 });
 
 //moon gate garland route
-app.get("/moon-gate-garland", function(req, res){
+app.get("/moon-gate-garland", function(req, res) {
   res.render("moon-gate-garland");
 });
 
 //rainbow arch route
-app.get("/rainbow-arch", function(req, res){
+app.get("/rainbow-arch", function(req, res) {
   res.render("rainbow-arch");
 });
 
 //balloon mosaic route
-app.get("/balloon-mosaic", function(req, res){
+app.get("/balloon-mosaic", function(req, res) {
   res.render("balloon-mosaic");
 });
 
 //add-ons route
-app.get("/add-ons", function(req, res){
+app.get("/add-ons", function(req, res) {
   res.render("add-ons");
 });
 
 //faq route
-app.get("/faq", function(req, res){
+app.get("/faq", function(req, res) {
   res.render("faq");
 });
 
 //testimonials route
-app.get("/testimonials", function(req, res){
+app.get("/testimonials", function(req, res) {
   res.render("testimonials");
 });
 
 //about route
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about");
 });
 
 //book route
-app.get("/book", function(req, res){
-  res.render("book");
+app.get("/book", function(req, res) {
+  res.render("book", {
+    data: {}
+  });
 });
 
 //post route for book form
-app.post("/home", function(req, res){
+app.post("/home", function(req, res) {
   res.render("home");
 });
 
-app.post("/book", function(req, res){
 
-  const message = new Message({
-    name: req.body.name,
-    email: req.body.email,
-    subject: req.body.subject,
-    event: req.body.eventDate,
-    product: req.body.productChoices,
-    response: req.body.response
-  });
-  message.save(function(err) {
-    if(!err){
-      res.redirect("/");
+app.post("/book",
+
+  //name validator
+  check("name", "Name required").trim().notEmpty().matches(/^[a-zA-Z ]*$/).withMessage("Only Characters with white space are allowed"),
+
+  //date validator
+  check("eventDate", "Date required").isISO8601().toDate(),
+
+  //email validator
+  check("email", "Email address required").notEmpty().normalizeEmail().isEmail().withMessage("Must be a valid email address"),
+
+  //subject validator
+  check("subject", "Subject required").notEmpty(),
+
+  //product choices validator
+  check("productChoices", "Product choices required").notEmpty(),
+
+  //message validator
+  check("response", "Message required").notEmpty(),
+
+  (req, res) => {
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      // return res.status(422).jsonp(errors.array())
+      const alert = errors.array()
+      res.render("book", {
+        alert,
+        data: req.body
+      })
+    } else {
+      const message = new Message({
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.subject,
+        event: req.body.eventDate,
+        product: req.body.productChoices,
+        response: req.body.response
+      });
+      message.save(function(err) {
+        if (!err) {
+          res.redirect("/");
+        }
+      });
     }
   });
-});
+
 
 const PORT = process.env.PORT || 3000;
 
--- public/js/header.js
+++ public/js/header.js
@@ -9,7 +9,7 @@ function myFunction() {
   }
 }
 
-//SSLIDESHOW NAVIGATION
+//SLIDESHOW NAVIGATION
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -41,92 +41,3 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
-
-// FORM
-const nameF = document.querySelector("#name");
-const emailF = document.querySelector("#email");
-const form = document.querySelector("#form");
-
-var nameFormat = /^[a-zA-Z\s, ]+$/;
-
-//returns true if the input is empty, false if not
-const isEmpty = value => value === "" ? true : false;
-
-//form validation
-const isEmailValid = (email) => {
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(email);
-};
-
-//check validations
-const checkName = () => {
-  const name = nameF.value.trim();
-
-  if (isEmpty(name)) {
-    showError(nameF, "Name field cannot be empty");
-    nameF.setAttribute("placeholder", '');
-  } else if (!name.match(nameFormat)) {
-    showError(nameF, "Please enter a Name");
-  } else {
-    showSuccess(nameF);
-  }
-  return name;
-};
-
-const checkEmail = () => {
-  const email = emailF.value.trim();
-
-  if (isEmpty(email)) {
-    showError(emailF, "Email field cannot be empty");
-    emailF.setAttribute('placeholder', '');
-  } else if (!isEmailValid(email)) {
-    showError(emailF, "Must enter an Email")
-    emailF.setAttribute('placeholder', 'email@example.com');
-  } else {
-    showSuccess(emailF);
-  }
-};
-
-const showError = (input, message) => {
-  const form = input.parentElement;
-  const error = form.querySelector("label");
-  const image = form.querySelector("img");
-
-  //add "error" class and css that goes along with it
-  input.classList.add("error");
-  image.classList.remove("errorIcon");
-  input.classList.remove("success");
-
-  //show the error message
-  error.textContent = message;
-}
-
-const showSuccess = (input) => {
-  const form = input.parentElement;
-  const success = form.querySelector("label");
-  const image = form.querySelector("img");
-
-  //add "error" class and css that goes along with it
-  input.classList.add("success");
-  image.classList.add("errorIcon");
-  input.classList.remove("error");
-
-  //Show the error message in the "small" selector
-  success.textContent = "";
-}
-
-//prevent form from submitting when there's an error
-form.addEventListener("submit", function(e) {
-  e.preventDefault();
-
-  checkEmail(),
-  checkName(),
-
-  nameF.addEventListener("blur", function(){
-    checkName();
-  });
-
-  emailF.addEventListener("blur", function(){
-    checkEmail();
-  });
-});
